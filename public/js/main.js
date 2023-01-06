a8d43c51bf7e645adb898151433c6ffaa77765f8
vic fir <vicfir@hotmail.fr>
2023-01-06 15:16:17 +0100

    switch pour la rage mana fleches

--- public/js/main.js
+++ public/js/main.js
@@ -19,16 +19,48 @@ let team = [instance.guerrier, instance.mage, instance.archer];
 while (instance.boss.pv > 0) {
     //Chacque perso tape a son tour
     team.forEach(element => {
-      if (instance.boss.pv > 0) {
-        console.log(`${element.nom} frappe ${instance.boss.nom}`);
-        instance.boss.pv -= element.pa;
-        if (instance.boss.pv <= 0) {
-            console.log(`${instance.boss.nom} est MORT`);
-        } else {
-            console.log(`${instance.boss.nom} a ${instance.boss.pv}PV`);
-        }       
-      }  
+        //tant que le boss est en vie
+        if (instance.boss.pv > 0) {
+            console.log(`${element.nom} frappe ${instance.boss.nom}`);
+            instance.boss.pv -= element.pa;
+            //energie/conso perso
+            switch (element) {
+                case instance.guerrier:
+                    if (element.rage < 4) {
+                        element.rage ++ ;
+                        element.pa = 10;
+                        console.log(`La rage de ${element.nom} est passée à ${element.rage}`);
+                    } else {
+                        element.rage = 0;
+                        element.pa *= 1.25;
+                        console.log(``);
+                    }
+                    
+                    break;
+                case instance.mage:
+                    element.mana -= 2 ;
+                    console.log(`Le mana de ${element.nom} est passé à ${element.mana}`);
+                    break;
+                case instance.archer:
+                    element.fleche -- ;
+                    console.log(`Il reste ${element.fleche} à ${element.nom}`);
+                    break;
+            
+                default:
+                    console.log("error");
+                    break;
+            }
+            //mort ou pv restant
+            if (instance.boss.pv <= 0) {
+                console.log(`${instance.boss.nom} est MORT`);
+            } else {
+                console.log(`${instance.boss.nom} a ${instance.boss.pv}PV`);
+            }
+        }
+
+        console.log("--------------------");
     });
+    
     //Le boss tape un perso aleatoire
     if (instance.boss.pv > 0) {
         let heroAleatoire = funct.randArr(team);
@@ -36,4 +68,5 @@ while (instance.boss.pv > 0) {
         heroAleatoire.pv -= instance.boss.pa;
         console.log(`Il reste ${heroAleatoire.pv}PV a ${heroAleatoire.nom}`);
     }
+    console.log('********************************************');
 }
