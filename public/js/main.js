4a66c61605d476185a7f06bf3fcebb08b3ff3a6c
vicfir <vicfir@hotmail.fr>
2023-01-08 18:39:06 +0100

    postures fonctionnels enigme bug

--- public/js/main.js
+++ public/js/main.js
@@ -30,24 +30,24 @@ while (instance.boss.pv > 0 && team.length != 0) {
         console.log(`${instance.guerrier.nom} PV:${instance.guerrier.pv} Rage:${instance.guerrier.rage} | ${instance.mage.nom} PV:${instance.mage.pv} Mana:${instance.mage.mana} | ${instance.archer.nom} PV:${instance.archer.pv} Flèches:${instance.archer.fleche} || ${instance.boss.nom} PV:${instance.boss.pv}`);
         console.log(".");
 
-        // //choix posture
-        // posture = prompt("Quelle posture voulez vous choisir pour ce tour : defense, attaque, normal");
+        //choix posture
+        posture = prompt("Quelle posture voulez vous choisir pour ce tour : defense, attaque, normal");
 
         //Chacque perso tape a son tour
         team.forEach(element => {
 
-            // //actifer posture combat
-            // if(posture == "defense" ) {
-            //     console.log("Posture defence");
-            //     element.defense("start");
-            // } else if (posture == "attaque") {
-            //     console.log("Posture attaque");
-            //     element.attaque("start");
-            // } else if (posture == "normal") {
-            //     console.log("Posture normal");
-            // } else {
-            //     console.log("Error posture combat");
-            // }
+            //actifer posture combat
+            if(posture == "defense" ) {
+                console.log("Posture defense");
+                element.defense("start");
+            } else if (posture == "attaque") {
+                console.log("Posture attaque");
+                element.attaque("start");
+            } else if (posture == "normal") {
+                console.log("Posture normal");
+            } else {
+                console.log("Error posture combat");
+            }
             
             //tant que le boss est en vie
             if (instance.boss.pv > 0) {
@@ -73,15 +73,15 @@ while (instance.boss.pv > 0 && team.length != 0) {
                         console.log("error");
                         break;
                 }
-                //lancement enigme boss -20%pv
-                if (instance.boss.pv <= pvBossBase/5){
-                    // console.log(enigme);
-                    instance.boss.enigme(team, enigme);
-                    if (instance.boss.enigme(team, enigme)) {
-                        team = [];
-                    }
-                    console.log(instance.boss.enigme);
-                }
+                // //lancement enigme boss -20%pv
+                // if (instance.boss.pv <= pvBossBase/5){
+                //     // console.log(enigme);
+                //     instance.boss.enigme(team, enigme);
+                //     if (instance.boss.enigme(team, enigme)) {
+                //         team = [];
+                //     }
+                //     console.log(instance.boss.enigme);
+                // }
                 //VICTOIRE
                 if (instance.boss.pv <= 0) {
                     console.log(".");
@@ -100,20 +100,20 @@ while (instance.boss.pv > 0 && team.length != 0) {
         funct.bastonBoss(team);
     }
 
-    // //desactiver posture combat hero
-    // if(posture == "defense" ) {
-    //     console.log("Posture supprime");
-    //     instance.guerrier.defense("end");
-    //     instance.mage.defense("end");
-    //     instance.archer.defense("end");
-    // } else if (posture == "attaque") {
-    //     console.log("Posture supprime");
-    //     instance.guerrier.attaque("end");
-    //     instance.mage.attaque("end");
-    //     instance.archer.attaque("end");
-    // } else {
-    //     console.log("Error posture combat");
-    // }
+    //desactiver posture combat hero
+    if(posture == "defense" ) {
+        console.log("Posture supprime");
+        instance.guerrier.defense("end");
+        instance.mage.defense("end");
+        instance.archer.defense("end");
+    } else if (posture == "attaque") {
+        console.log("Posture supprime");
+        instance.guerrier.attaque("end");
+        instance.mage.attaque("end");
+        instance.archer.attaque("end");
+    } else {
+        console.log("Error posture combat");
+    }
 
     //DEFAITE
     if (team.length == 0) {
